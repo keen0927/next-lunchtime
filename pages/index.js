0f04b661c023ff5d1ef1ce5db3fa9b4e520f7e60
KEEN <kyh0927@live.co.kr>
2020-03-06 00:33:59 +0900

    next

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,48 @@
-import Layout from '../components/Layout';
-import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
-// const PostLink = props => (
-//     <li>
-//         <Link href="/p/[id]" as={`/p/${props.id}`}>
-//             <a>{props.id}</a>
-//         </Link>
-//     </li>
-// );
-
-// export default function Index() {
-//     return (
-//         <Layout>
-//             <h1>Posts</h1>
-//             <ul>
-//                 <PostLink id="1" />
-//                 <PostLink id="2" />
-//                 <PostLink id="3" />
-//             </ul>
-//         </Layout>
-//     )
-// }
-
-const Index = props => (
-    <Layout>
-        <h1>TV SHOW</h1>
-        <ul>
-            {props.shows.map(show => (
-                <li key={show.id}>
-                    <Link href="/p/[id]" as={`/p/${show.id}`}>
-                        <a>{show.name}</a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-        <style jsx>{`
-            li {
-                list-style: none;
-                padding: 0;
-                margin: 0;
-            }
-            a {
-                color: rgba(0,0,0,0.87);
-                transition: color 0.5s;
-            }
-            a:hover {
-                color: pink;
-            }
-        `}</style>
-    </Layout>
-);
-
-Index.getInitialProps = async function() {
-    const url = 'https://api.tvmaze.com/search/shows?q=batman';
-    const res = await fetch(url);
-    const data = await res.json();
-
-    console.log(`Data Count : ${data.length}`);
-
-    return {
-        shows: data.map(entry => entry.show)
-    };
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
 }
 
-export default Index;
+export default function Index() {
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher
+  );
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.author`
+  const author = data?.author;
+  let quote = data?.quote;
+
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
+
+  return (
+    <main className="center">
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
+
+      <style jsx>{`
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
+        }
+        .quote {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
+        }
+        .author {
+          font-family: sans-serif;
+          color: #559834;
+          font-size: 20px;
+        }
+      `}</style>
+    </main>
+  );
+}
